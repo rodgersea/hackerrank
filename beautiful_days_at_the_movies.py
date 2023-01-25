9f068538c8447c25f38bc0632f79dd7cbc3abef4
rodgersea <elliottarodgers@gmail.com>
2023-01-25 11:58:02 -0500

    beautiful days solved

---
+++ beautiful_days_at_the_movies.py
@@ -0,0 +1,19 @@
+
+def beautifuldays(i, j, k):
+    def isbeautiful(num):
+        numstring = str(num)
+        numreversed = ''
+        for a in numstring[::-1]:
+            numreversed += a
+        numreversed = int(numreversed.lstrip('0'))
+        print(numreversed)
+        return numreversed
+    print(f'i: {i}, j: {j}, k: {k}')
+    numdays = 0
+    for x in range(i, j+1):
+        print(x)
+        if abs(x-isbeautiful(x))%k == 0:
+            numdays += 1
+    return numdays
+
+beautifuldays(20, 23, 6)
